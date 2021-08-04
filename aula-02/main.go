1165f23ecbbfb564ed9fa57ae5a809e54338efad
filipetomita <filipe.tomita@gmail.com>
2021-08-04 18:27:49 -0300

    fix lesson-02

--- aula-02/main.go
+++ aula-02/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Olá do arquivo main do novo módulo")
 	auxiliar.Escrever()
+
+	erro := checkmail.ValidadeFormat("filipe.tomita@gmailcom")
+	fmt.Println(erro)
 }
